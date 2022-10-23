27b568cad079de9f5fab3403a59d408e2e111a00
Raphael Nascimento <raphael.rsn.123@gmail.com>
2022-10-23 19:51:30 -0300

    :sparkles: Create logs

--- app/operations/send_user_email/call_operation.rb
+++ app/operations/send_user_email/call_operation.rb
@@ -14,13 +14,25 @@ class SendUserEmail::CallOperation
   def find_template_by_event!
     case params[:event]
     when 'quote_update'
+      logs(params[:application], params[:event])
       SendUserEmailMailer.with(params).quote_update.deliver_later
     when 'forgot_password'
+      logs(params[:application], params[:event])
       SendUserEmailMailer.with(params).forgot_password.deliver_later
     when 'new_agreement'
+      logs(params[:application], params[:event])
       SendUserEmailMailer.with(params).new_agreement.deliver_later
     else
       context.fail!(errors: 'Not send email')
     end
+  end
+
+  def logs(app_name, event)
+    application = Application.find_by_name(app_name)
+    event_notification = EventsNotification.find_by_event(event)
+    log = LogsNotification.new
+    log.application_id = application.id
+    log.events_notification_id = event_notification.id
+    log.save!
   end
 end
